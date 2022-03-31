6156e176de30d01e49afc8f8fca2bc6d95de2607
Vicki <vicki@mixen.dk>
2022-03-31 09:11:50 +0200

    version change

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 import * as path from 'path'
 import * as net from 'net'
-import { window, workspace, ExtensionContext, Task, tasks, ShellExecution, OutputChannel, Uri } from 'vscode'
+import { window, workspace, ExtensionContext, Task, tasks, ShellExecution, OutputChannel, Uri, Range, Selection } from 'vscode'
 import * as cp from 'child_process'
-import { LanguageClient, LanguageClientOptions, StreamInfo } from 'vscode-languageclient'
+import { LanguageClient, LanguageClientOptions, StreamInfo, Middleware, WorkspaceChange, Disposable} from 'vscode-languageclient'
 import * as semver from 'semver'
 import * as execa from 'execa'
 import * as vscode from 'vscode'
@@ -11,7 +11,9 @@ let client: LanguageClient
 let proc: cp.ChildProcess
 let logger: OutputChannel
 
-const versionRequirement = ">=1.10.1"
+const versionRequirement1 = ">=1.10.1"
+const versionRequirement2 = ">=1.11.0"
+let languageServerVersion: String
 const IsWindows = ( process.platform === "win32" )
 
 function getConfigValue( value: string ): any {
@@ -37,8 +39,13 @@ async function checkRequiredJolieVersion():Promise<void> {
 		const result = stderr.match(/Jolie\s+(\d+\.\d+\.\d+).+/)
 		if (result.length > 1) {
 			const jolieVersion = result[1]
-			if( !semver.satisfies(jolieVersion, versionRequirement) ) {
-				window.showErrorMessage(`This extension requires Jolie version ${versionRequirement}, whereas your version is ${jolieVersion}. Some features may not work correctly. Please consider updating your Jolie installation.`)
+			if( !semver.satisfies(jolieVersion, versionRequirement1) ) {
+				window.showErrorMessage(`This extension requires Jolie version ${versionRequirement1} or newer, whereas your version is ${jolieVersion}. Some features may not work correctly. Please consider updating your Jolie installation.`)
+			}
+			else if (semver.satisfies(jolieVersion, versionRequirement2)){
+				languageServerVersion = "0.3.0"
+			} else {
+				languageServerVersion = "0.2.1"
 			}
 		} else {
 			window.showErrorMessage(`Could not detect the version of the Jolie interpreter. Output of \"jolie --version\": ${stderr}`)
@@ -73,6 +80,29 @@ export async function activate(context: ExtensionContext) {
 	const tcpPort: number = getConfigValue( 'jolie.languageServer.tcpPort' )
 
 	log( "Activating Jolie Language Server" )
+
+	vscode.commands.registerCommand("vscode-jolie.enableCodeLens", () => {
+        workspace.getConfiguration("vscode-jolie").update("enableCodeLens", true, true);
+    });
+
+    vscode.commands.registerCommand("vscode-jolie.disableCodeLens", () => {
+        workspace.getConfiguration("vscode-jolie").update("enableCodeLens", false, true);
+    });
+
+	let disposable = vscode.commands.registerCommand('vscode-jolie.executeHoverProvider', () => {
+		const range = new Range(1, 1, 1, 10);
+		const decoration = window.createTextEditorDecorationType({color: "green", borderColor: "purple", borderWidth: "1px", border: "solid", overviewRulerColor: "blue", overviewRulerLane: vscode.OverviewRulerLane.Right});
+		window.activeTextEditor.setDecorations(decoration, [range])
+		//window.activeTextEditor.selection = new Selection(1, 1, 1, 10);
+		window.showInformationMessage("vscode-jolie.executeHoverProvider has been called")
+	})
+
+	let nameConvention = vscode.commands.registerCommand('vscode-jolie.nameConvention', () => {
+		window.showInformationMessage("nameConvention has been called")
+	})
+	
+
+	context.subscriptions.push(disposable, nameConvention)
 	
 	const serverOptions = () => new Promise<StreamInfo>( (resolve, reject) => {
 		const serverPath = vscode.extensions.getExtension("jolie.vscode-jolie").extensionPath
@@ -81,7 +111,9 @@ export async function activate(context: ExtensionContext) {
 		
 		if(IsWindows){
 			command = 'cmd.exe'
-			args = ['/C', 'npx', '--yes', '--package', '@jolie/languageserver', 'joliels', `${tcpPort}`]
+			//args = ['/C', 'npx', '--yes', '--package', '@jolie/languageserver', '-v #'+languageServerVersion, 'joliels', `${tcpPort}`]
+			args = ['/C', 'jolie.bat', 'c:/Users/vicki/Desktop/languageserver/launcher.ol', `${tcpPort}`]
+			//args = ['/C', 'jolie.bat', '--trace', 'c:/Users/vicki/Desktop/languageserver/launcher.ol', `${tcpPort}`]
 		} else {
 			command = 'npx'
 			args = ['--package', '@jolie/languageserver', 'joliels', `${tcpPort}`]
@@ -100,6 +132,9 @@ export async function activate(context: ExtensionContext) {
 				//if the jolie process has started, we connect the client to the socket and resolve the childProcess
 				const socket = net.createConnection( { port : tcpPort, host:'localhost' } )
 				resolve({ reader: socket, writer: socket })
+			}
+			if(message.includes("Rename abandonned.")){
+				window.showErrorMessage( message )
 			}
 			log(`Jolie says: ${message}`)
 		})
@@ -124,6 +159,27 @@ export async function activate(context: ExtensionContext) {
 			// configurationSection: 'jolie',
 			fileEvents: workspace.createFileSystemWatcher('**/*.{ol,iol}')
 		},
+		middleware: { // Documents need to be saved, as the rename function uses the text saved on disc
+			provideRenameEdits: async (document, position, newName, token, next) => {
+				let allSaved = workspace.saveAll(false)
+				;(await allSaved).valueOf
+				if(allSaved){ //allSaved is false if some documents couldn't be saved
+					return next(document, position, newName, token)
+				} else {
+					window.showInformationMessage("Some documents could not be saved before, rename was abandonned.")
+				}
+			},
+			resolveCodeLens: async (codeLens, token, next) => { // this is not called for some reason
+				window.showInformationMessage("trying to make codelens resolve!!!!!!!!!!!!!!!!!!!!!!");
+				return next(codeLens, token)
+
+			},
+			provideCodeLenses:async (document, token, next) => {
+				window.showInformationMessage("provideCodeLenses!");
+				return next(document, token)
+				
+			}
+		},
 		uriConverters: {
 			// See https://github.com/Microsoft/vscode-languageserver-node/issues/105
 			code2Protocol: uri =>
@@ -140,6 +196,7 @@ export async function activate(context: ExtensionContext) {
 	)
 
 	client.start()
+
 }
 
 export async function deactivate(): Promise<void> | undefined {
