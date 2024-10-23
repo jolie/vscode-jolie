858bfd3bb81803b678fbbec538bd1619646fdc0a
Narongrit Unwerawattana <narongrit.kie@gmail.com>
2024-10-23 20:06:02 +0200

    update dependencies versions

--- src/extension.ts
+++ src/extension.ts
@@ -1,181 +1,256 @@
-import * as path from 'path'
-import * as net from 'net'
-import { window, workspace, ExtensionContext, Task, tasks, ShellExecution, OutputChannel, Uri} from 'vscode'
-import * as cp from 'child_process'
-import { LanguageClient, LanguageClientOptions, StreamInfo} from 'vscode-languageclient'
-import * as semver from 'semver'
-import * as execa from 'execa'
-import * as vscode from 'vscode'
+import { dirname } from "path";
+import * as semver from "semver";
+import {
+  ExtensionContext,
+  OutputChannel,
+  ShellExecution,
+  Task,
+  tasks,
+  Uri,
+  window,
+  workspace,
+  TaskScope,
+} from "vscode";
 
-let client: LanguageClient
-let proc: cp.ChildProcess
-let logger: OutputChannel
+import {
+  CloseAction,
+  CloseHandlerResult,
+  ErrorAction,
+  ErrorHandlerResult,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  Trace,
+} from "vscode-languageclient/node";
+import LanguageServerProcess, { ServerError } from "./ls_process";
+import { spawnSync } from "child_process";
+import { createServer, Socket } from "net";
+const isWindows = process.platform === "win32";
 
-const versionRequirement = [[">=1.10.1", "^0.2.1"], [">=1.11.0", "^1.0.0"]]
-let languageServerVersion: String
-const IsWindows = ( process.platform === "win32" )
+let server: LanguageServerProcess;
+let client: LanguageClient;
+let logger: OutputChannel;
+let socket: Socket;
 
-function getConfigValue( value: string ): any {
-	return workspace.getConfiguration().get( value )
+const versionRequirement = [
+  [">=1.10.1", "^0.2.1"],
+  [">=1.11.0", "^1.0.0"],
+];
+let languageServerVersion: string;
+
+function createRunTask(title: string): Task {
+  const file = window.activeTextEditor!.document.fileName;
+  const dir = dirname(file);
+  return new Task(
+    { type: "run", task: "runJolie" },
+    TaskScope.Workspace,
+    title,
+    "Jolie",
+    new ShellExecution(`jolie "${file}"`, { cwd: dir }),
+    []
+  );
 }
 
-function createRunTask( title: string ): Task {
-	let file = window.activeTextEditor.document.fileName;
-	let dir = path.dirname( file )
-	return new Task( 
-		{ type: "run", task: "runJolie" },
-		title,
-		"Jolie", 
-		new ShellExecution( `jolie "${file}"`, { cwd : dir } ), 
-		[]
-	)
-}
-
-async function checkRequiredJolieVersion():Promise<void> {
-	try {
-		const p = await execa('jolie', ['--version'])
-		const stderr = p.stderr
-		const result = stderr.match(/Jolie\s+(\d+\.\d+\.\d+).+/)
-		if (result.length > 1) {
-			const jolieVersion = result[1]
-			var index = versionRequirement.length-1
-			for (let i = index; i >= 0; i--) {
-				if (semver.satisfies(jolieVersion, versionRequirement[i][0])){
-					languageServerVersion = versionRequirement[i][1]
-					break
-				}
-			}
-			if(!languageServerVersion){
-				window.showErrorMessage(`This extension requires Jolie version ${versionRequirement[0][0]} or newer, whereas your version is ${jolieVersion}. Some features may not work correctly. Please consider updating your Jolie installation.`)
-			
-			}
-		} else {
-			window.showErrorMessage(`Could not detect the version of the Jolie interpreter. Output of \"jolie --version\": ${stderr}`)
-		}
-	} catch(err) {
-		window.showErrorMessage('Could not start the Jolie interpreter. Please check that the jolie executable is installed.')
-	}
+async function checkRequiredJolieVersion(): Promise<void> {
+  let p;
+  try {
+    p = spawnSync("jolie --version", { encoding: "utf8", shell: true });
+  } catch (err) {
+    const e = err as Error;
+    throw new Error(
+      "Could not start the Jolie interpreter. Please check that the jolie executable is installed.",
+      { cause: e }
+    );
+  }
+  const result = p.stderr.match(/Jolie\s+(\d+\.\d+\.\d+).+/);
+  if (Array.isArray(result) && result.length > 1) {
+    const jolieVersion = result![1];
+    const index = versionRequirement.length - 1;
+    for (let i = index; i >= 0; i--) {
+      if (semver.satisfies(jolieVersion, versionRequirement[i][0])) {
+        languageServerVersion = versionRequirement[i][1];
+        break;
+      }
+    }
+    if (!languageServerVersion) {
+      throw new Error(
+        `This extension requires Jolie version ${versionRequirement[0][0]} or newer, whereas your version is ${jolieVersion}. Some features may not work correctly. Please consider updating your Jolie installation.`
+      );
+    }
+  } else {
+    throw new Error(
+      `Could not detect the version of the Jolie interpreter. Output of "jolie --version": ${p.stderr}`
+    );
+  }
 }
 
 function registerTasks() {
-	tasks.registerTaskProvider( "run", {
-		provideTasks: () => {
-			return [ createRunTask( "Run current Jolie program" ) ];
-		},
-		resolveTask( task : Task ): Task | undefined {
-			return task
-		}
-	})
+  tasks.registerTaskProvider("run", {
+    provideTasks: () => {
+      return [createRunTask("Run current Jolie program")];
+    },
+    resolveTask(task: Task): Task | undefined {
+      return task;
+    },
+  });
 }
 
-function log( message: string ) {
-	if( getConfigValue( 'jolie.languageServer.showDebugMessages' ) ){
-		logger.appendLine( message )
-	}
+function log(message: string) {
+  if (
+    workspace.getConfiguration().get("jolie.languageServer.showDebugMessages")
+  ) {
+    logger.appendLine("vscode-jolie lsp client: " + message);
+  }
 }
 
-export async function activate(context: ExtensionContext) {
-	await checkRequiredJolieVersion()
-	registerTasks()
-	
-	logger = window.createOutputChannel( 'Jolie LSP Client' )
-	const tcpPort: number = getConfigValue( 'jolie.languageServer.tcpPort' )
+async function checkPortAvailable(port: number): Promise<boolean> {
+  return new Promise((resolve) => {
+    const s = createServer();
 
-	log( "Activating Jolie Language Server" )
-	
-	const serverOptions = () => new Promise<StreamInfo>( (resolve, reject) => {
-		const serverPath = vscode.extensions.getExtension("jolie.vscode-jolie").extensionPath
-		var command: string
-		var args: string[]
-		
-		if(IsWindows){
-			command = 'cmd.exe'
-			args = ['/C', 'npx', '--package=@jolie/languageserver@'+languageServerVersion, 'joliels', `${tcpPort}`]
-		} else {
-			command = 'npx'
-			args = ['--package=@jolie/languageserver@'+languageServerVersion, 'joliels', `${tcpPort}`]
-		}
+    s.once("error", function (err) {
+      if (err.message.includes("EADDRINUSE")) {
+        resolve(false);
+      }
+    });
+    s.once("listening", function () {
+      s.close(() => resolve(true));
+    });
 
-		log(`starting "${command} ${args.join(' ')}"`)
-		proc = cp.spawn(command, args)
-
-		proc.on("error", (err)=>{
-			log(`error: ${String(err)}`)
-		})
-
-		proc.stdout.on('data', (out) => {
-			const message = String(out)
-			if ( message.includes( "Jolie Language Server started" ) ) {
-				//if the jolie process has started, we connect the client to the socket and resolve the childProcess
-				const socket = net.createConnection( { port : tcpPort, host:'localhost' } )
-				resolve({ reader: socket, writer: socket })
-			}
-			if(message.includes("Rename abandoned.")){
-				window.showErrorMessage( message )
-			}
-			log(`Jolie says: ${message}`)
-		})
-		proc.stderr.on('data', (out) => {
-			let s = String( out )
-			if( s.includes( "service initialisation failed" ) ){
-				window.showErrorMessage( s )
-			}
-			if( s.includes( "Address already in use" ) ){
-				window.showErrorMessage( s )
-				window.showInformationMessage( "Please check that the TCP port " + tcpPort + " of the local machine is free. " + 
-				"Alternatively, you can change the TCP port number under the Jolie Language Support extension configuration. " +
-				"After the change, remember to reboot your editor." )
-			}
-			log( s )
-		})
-	})
-
-	const clientOptions: LanguageClientOptions = {
-		documentSelector: [{ scheme: 'file', language: 'jolie' }, { scheme: 'untitled', language: 'jolie' }],
-		synchronize: {
-			// configurationSection: 'jolie',
-			fileEvents: workspace.createFileSystemWatcher('**/*.{ol,iol}')
-		},
-		middleware: { // Documents need to be saved, as the rename function uses the text saved on disc
-			provideRenameEdits: async (document, position, newName, token, next) => {
-				let allSaved = workspace.saveAll(false)
-				;(await allSaved).valueOf
-				if(allSaved){ //allSaved is false if some documents couldn't be saved
-					return next(document, position, newName, token)
-				} else {
-					window.showInformationMessage("Some documents could not be saved before, rename was abandonned.")
-				}
-			}
-		},
-		uriConverters: {
-			// See https://github.com/Microsoft/vscode-languageserver-node/issues/105
-			code2Protocol: uri =>
-				IsWindows ? uri.toString().replace('%3A', ':') : uri.toString(),
-			protocol2Code: str => Uri.parse(str),
-		}
-	}
-
-	client = new LanguageClient(
-		'vscode-jolie',
-		'Jolie Language Server',
-		serverOptions,
-		clientOptions
-	)
-
-	client.start()
-
+    s.listen(port);
+  });
 }
 
-export async function deactivate(): Promise<void> | undefined {
-	if ( !client ) {
-		if ( proc ) {
-			proc.kill( 'SIGINT' )
-		}
-		return undefined	
-	}
-	return client.stop().then( () => {
-		if ( proc ) {
-			proc.kill( 'SIGINT' )
-		}
-	} )
+function createLSServer(tcpPort: number) {
+  server = new LanguageServerProcess(
+    languageServerVersion,
+    tcpPort,
+    window.createOutputChannel("Jolie LSP Server"),
+    isWindows
+  );
+
+  server.on("error", (err: ServerError) => {
+    const message =
+      "JolieLS: error occur " +
+      err.error.message +
+      ", try to reconnect = " +
+      err.should_retry;
+    window.showErrorMessage(message);
+    if (!err.should_retry) {
+      client.stop();
+    }
+  });
+
+  return server;
+}
+
+export async function activate(_context: ExtensionContext) {
+  await checkRequiredJolieVersion();
+  registerTasks();
+
+  logger = window.createOutputChannel("Jolie LSP Client");
+  const tcpPort: number = <number>(
+    workspace.getConfiguration().get("jolie.languageServer.tcpPort")
+  );
+
+  const isPortAvailable = await checkPortAvailable(tcpPort);
+
+  socket = new Socket();
+
+  const serverStreamInfo: ServerOptions = () => {
+    return new Promise((resolve) => {
+      if (isPortAvailable && process.env.LANGUAGE_SERVER !== "external") {
+        log("Start Jolie Language Server");
+        const server = createLSServer(tcpPort);
+        server.start();
+        server.on("ready", () => {
+          socket.connect({ host: "localhost", port: tcpPort });
+          resolve({ reader: socket, writer: socket });
+        });
+      } else {
+        log("Connect to Jolie Language Server");
+        socket.connect({ host: "localhost", port: tcpPort });
+        resolve({ reader: socket, writer: socket });
+      }
+    });
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      { scheme: "file", language: "jolie" },
+      { scheme: "untitled", language: "jolie" },
+    ],
+    synchronize: {
+      // configurationSection: 'jolie',
+      fileEvents: workspace.createFileSystemWatcher("**/*.{ol,iol}"),
+    },
+    middleware: {
+      // Documents need to be saved, as the rename function uses the text saved on disc
+      provideRenameEdits: async (document, position, newName, token, next) => {
+        const allSaved = await workspace.saveAll(false);
+        if (allSaved) {
+          //allSaved is false if some documents couldn't be saved
+          return next(document, position, newName, token);
+        } else {
+          window.showInformationMessage(
+            "Some documents could not be saved before, rename was abandoned."
+          );
+        }
+      },
+    },
+    uriConverters: {
+      // See https://github.com/Microsoft/vscode-languageserver-node/issues/105
+      code2Protocol: (uri) => {
+        return isWindows ? uri.toString().replace("%3A", ":") : uri.toString();
+      },
+      protocol2Code: (str) => {
+        return Uri.parse(str);
+      },
+    },
+    errorHandler: {
+      closed(): CloseHandlerResult {
+        return {
+          action: CloseAction.DoNotRestart,
+        };
+      },
+
+      error(error): ErrorHandlerResult {
+        if (error.message.includes("ECONNREFUSED")) {
+          const message = `Unable to connect to Jolie Language Server, please make sure the Jolie Language Server is running on port ${tcpPort}.`;
+          log(message);
+          return {
+            action: ErrorAction.Shutdown,
+            message: message,
+            handled: true,
+          };
+        } else {
+          log("unhandled error");
+          return {
+            action: ErrorAction.Continue,
+            message: error.message,
+          };
+        }
+      },
+    },
+  };
+  client = new LanguageClient(
+    "vscode-jolie",
+    "Jolie LanguageClient",
+    serverStreamInfo,
+    clientOptions
+  );
+  if (
+    workspace.getConfiguration().get("jolie.languageServer.showDebugMessages")
+  ) {
+    await client.setTrace(Trace.Verbose);
+  }
+  await client.start();
+
+  return;
+}
+
+export async function deactivate(): Promise<void | undefined> {
+  socket.removeAllListeners();
+  socket.destroy();
+  server?.kill();
+  return client?.stop();
 }
---
+++ src/ls_process.ts
@@ -0,0 +1,139 @@
+import { ChildProcessWithoutNullStreams, spawn } from "child_process";
+import { EventEmitter } from "node:events";
+import { OutputChannel, window } from "vscode";
+
+export interface ServerError {
+  error: Error;
+  description: string;
+  should_retry: boolean;
+}
+
+export default class LanguageServerProcess extends EventEmitter {
+  command: string;
+  args: string[];
+  port: number;
+  logger: OutputChannel;
+  proc: ChildProcessWithoutNullStreams | undefined;
+
+  public constructor(
+    languageServerVersion: string,
+    port: number,
+    logger: OutputChannel,
+    isWindows: boolean
+  ) {
+    super({ captureRejections: true });
+    const languageServer = process.env.LANGUAGE_SERVER;
+    if (!languageServer) {
+      if (isWindows) {
+        this.command = "cmd.exe";
+        this.args = [
+          "/C",
+          "npx",
+          "--package=@jolie/languageserver@" + languageServerVersion,
+          "joliels",
+          `${port}`,
+        ];
+      } else {
+        this.command = "npx";
+        this.args = [
+          "--package=@jolie/languageserver@" + languageServerVersion,
+          "joliels",
+          `${port}`,
+        ];
+      }
+    } else {
+      // languageServer is a path to launcher
+      if (isWindows) {
+        this.command = "cmd.exe";
+        this.args = ["/C", "jolie", languageServer, `${port}`];
+      } else {
+        this.command = "jolie";
+        this.args = ["--trace", languageServer, `${port}`];
+      }
+    }
+    logger.appendLine(
+      "Start LS Process with command " +
+        this.command +
+        " " +
+        this.args.join(" ")
+    );
+    this.logger = logger;
+    this.port = port;
+  }
+
+  onReceiveFromJolie = (message: string): void => {
+    if (message.includes("Jolie Language Server started")) {
+      this.emit("ready");
+    } else if (!this._shouldIgnoreMessage(message)) {
+      const error = this._processErrorMessage(message);
+      if (error) {
+        this.emit("error", error);
+        this.logger.appendLine(message);
+      } else {
+        this.logger.appendLine(message);
+      }
+    }
+  };
+
+  _ignoreMessages: string[] = [
+    "java.io.IOException: java.nio.channels.IllegalBlockingModeException",
+  ];
+
+  _shouldIgnoreMessage(message: string) {
+    for (const ignore of this._ignoreMessages) {
+      if (message.includes(ignore)) {
+        return true;
+      }
+    }
+  }
+
+  _processErrorMessage = (errorMessage: string): ServerError | undefined => {
+    const error = new Error(errorMessage);
+    if (errorMessage.includes("Rename abandoned.")) {
+      return {
+        error,
+        description: "Rename abandoned.",
+        should_retry: false,
+      };
+    }
+    if (errorMessage.includes("service initialisation failed")) {
+      return {
+        error,
+        description: "Jolie Language Server is not running.",
+        should_retry: false,
+      };
+    }
+    if (errorMessage.includes("Address already in use")) {
+      return {
+        error,
+        description: `Please check that the TCP port ${this.port} of the local machine is free. Alternatively, you can change the TCP port number under the Jolie Language Support extension configuration. After the change, remember to reboot your editor.`,
+        should_retry: false,
+      };
+    }
+
+    return;
+  };
+
+  start = () => {
+    // spawn a new process with the command and arguments
+    this.proc = spawn(this.command, this.args);
+    this.proc.stdout.setEncoding("utf-8");
+    this.proc.stderr.setEncoding("utf-8");
+    // listen to the stdout and stderr events
+    this.proc.stdout.on("data", this.onReceiveFromJolie);
+    this.proc.stderr.on("data", this.onReceiveFromJolie);
+    // listen to the exit event
+    this.proc.on("exit", (_code) => {
+      this.emit("end");
+    });
+  };
+
+  kill = () => {
+    if (this.proc) {
+      this.proc.stdin.destroy();
+      this.proc.stdout.destroy();
+      this.proc.stderr.destroy();
+      this.proc.kill("SIGINT");
+    }
+  };
+}
